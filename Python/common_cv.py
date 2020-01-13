f68fa5891530229dcaccd5e7c4db3f6bfcddc3bd
Aaron McClintock <41651816+Spectavi@users.noreply.github.com>
2020-01-13 12:00:01 -0800

    PEP fixes for common_cv.py

--- Python/common_cv.py
+++ Python/common_cv.py
@@ -1,53 +1,48 @@
-#/usr/bin/env python
+import os
 
-'''
-This module contais some common routines used by other samples.
-'''
+from contextlib import contextmanager
+import cv2
+import itertools as it
+import numpy as np
 
 import config
 
-import numpy as np
-import cv2
-import os
-from contextlib import contextmanager
-import itertools as it
+image_extensions = ['.bmp', '.jpg', '.jpeg', '.png', '.tif', '.tiff', '.pbm',
+                    '.pgm', '.ppm']
 
-image_extensions = ['.bmp', '.jpg', '.jpeg', '.png', '.tif', '.tiff', '.pbm', '.pgm', '.ppm']
+'''
+    This module contains some common routines used by other samples.
+'''
+
 
 class Bunch(object):
     def __init__(self, **kw):
         self.__dict__.update(kw)
+
     def __str__(self):
         return str(self.__dict__)
+
 
 def splitfn(fn):
     path, fn = os.path.split(fn)
     name, ext = os.path.splitext(fn)
     return path, name, ext
 
+
 def anorm2(a):
-    if False:
-        print "a = %s" % str(a)
-
-        res = a * a
-        print "a * a = %s" % str(res)
-        res = res.sum(-1)
-        print "(a * a).sum(-1) = %s" % str(res)
-
-        res = (a * a).sum(-1)
-        print "res = %s" % str(res)
-        return res
-
     return (a * a).sum(-1)
 
-def anorm(a):
-    return np.sqrt( anorm2(a) )
 
-def homotrans(H, x, y):
-    xs = H[0, 0]*x + H[0, 1]*y + H[0, 2]
-    ys = H[1, 0]*x + H[1, 1]*y + H[1, 2]
-    s  = H[2, 0]*x + H[2, 1]*y + H[2, 2]
+def anorm(a):
+    return np.sqrt(anorm2(a))
+
+
+def homotrans(h, x, y):
+    xs = h[0, 0] * x + h[0, 1] * y + h[0, 2]
+    ys = h[1, 0] * x + h[1, 1] * y + h[1, 2]
+    s  = h[2, 0] * x + h[2, 1] * y + h[2, 2]
     return xs/s, ys/s
+
 
 def to_rect(a):
     a = np.ravel(a)
@@ -55,41 +50,49 @@ def to_rect(a):
         a = (0, 0, a[0], a[1])
     return np.array(a, np.float64).reshape(2, 2)
 
+
 def rect2rect_mtx(src, dst):
     src, dst = to_rect(src), to_rect(dst)
     cx, cy = (dst[1] - dst[0]) / (src[1] - src[0])
     tx, ty = dst[0] - src[0] * (cx, cy)
-    M = np.float64([[ cx,  0, tx],
-                    [  0, cy, ty],
-                    [  0,  0,  1]])
-    return M
+    m = np.float64([[cx,  0, tx],
+                    [0, cy, ty],
+                    [0,  0,  1]])
+    return m
 
 
-def lookat(eye, target, up = (0, 0, 1)):
+def lookat(eye, target, up=(0, 0, 1)):
     fwd = np.asarray(target, np.float64) - eye
     fwd /= anorm(fwd)
     right = np.cross(fwd, up)
     right /= anorm(right)
     down = np.cross(fwd, right)
-    R = np.float64([right, down, fwd])
-    tvec = -np.dot(R, eye)
-    return R, tvec
+    r = np.float64([right, down, fwd])
+    tvec = -np.dot(r, eye)
+    return r, tvec
 
-def mtx2rvec(R):
-    w, u, vt = cv2.SVDecomp(R - np.eye(3))
-    p = vt[0] + u[:,0]*w[0]    # same as np.dot(R, vt[0])
+
+def mtx2rvec(r):
+    w, u, vt = cv2.SVDecomp(r - np.eye(3))
+    p = vt[0] + u[:, 0]*w[0]    # same as np.dot(R, vt[0])
     c = np.dot(vt[0], p)
     s = np.dot(vt[1], p)
     axis = np.cross(vt[0], vt[1])
     return axis * np.arctan2(s, c)
 
+
 def draw_str(dst, (x, y), s):
     if config.OCV_OLD_PY_BINDINGS:
-        cv2.putText(dst, s, (x+1, y+1), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 0), thickness = 2, lineType=cv2.CV_AA)
-        cv2.putText(dst, s, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), lineType=cv2.CV_AA)
+        cv2.putText(dst, s, (x + 1, y + 1), cv2.FONT_HERSHEY_PLAIN, 1.0,
+                    (0, 0, 0), thickness=2, lineType=cv2.CV_AA)
+        cv2.putText(dst, s, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.0,
+                    (255, 255, 255), lineType=cv2.CV_AA)
     else:
-        cv2.putText(dst, s, (x+1, y+1), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 0), thickness = 2, lineType=cv2.LINE_AA)
-        cv2.putText(dst, s, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), lineType=cv2.LINE_AA)
+        cv2.putText(dst, s, (x + 1, y + 1), cv2.FONT_HERSHEY_PLAIN, 1.0,
+                    (0, 0, 0), thickness=2, lineType=cv2.LINE_AA)
+        cv2.putText(dst, s, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.0,
+                    (255, 255, 255), lineType=cv2.LINE_AA)
+
 
 class Sketcher:
     def __init__(self, windowname, dests, colors_func):
@@ -119,14 +122,15 @@ class Sketcher:
 
 
 # palette data from matplotlib/_cm.py
-_jet_data =   {'red':   ((0., 0, 0), (0.35, 0, 0), (0.66, 1, 1), (0.89,1, 1),
-                         (1, 0.5, 0.5)),
-               'green': ((0., 0, 0), (0.125,0, 0), (0.375,1, 1), (0.64,1, 1),
-                         (0.91,0,0), (1, 0, 0)),
-               'blue':  ((0., 0.5, 0.5), (0.11, 1, 1), (0.34, 1, 1), (0.65,0, 0),
-                         (1, 0, 0))}
+_jet_data = {'red': ((0., 0, 0), (0.35, 0, 0), (0.66, 1, 1), (0.89, 1, 1),
+                     (1, 0.5, 0.5)),
+             'green': ((0., 0, 0), (0.125, 0, 0), (0.375, 1, 1), (0.64, 1, 1),
+                       (0.91, 0, 0), (1, 0, 0)),
+             'blue': ((0., 0.5, 0.5), (0.11, 1, 1), (0.34, 1, 1), (0.65, 0, 0),
+                      (1, 0, 0))}
 
-cmap_data = { 'jet' : _jet_data }
+cmap_data = {'jet': _jet_data}
+
 
 def make_cmap(name, n=256):
     data = cmap_data[name]
@@ -143,31 +147,37 @@ def make_cmap(name, n=256):
         channels.append(ch)
     return np.uint8(np.array(channels).T*255)
 
+
 def nothing(*arg, **kw):
     pass
+
 
 def clock():
     return cv2.getTickCount() / cv2.getTickFrequency()
 
+
 @contextmanager
-def Timer(msg):
-    print msg, '...',
+def timer(msg):
+    print(msg, '...')
     start = clock()
     try:
         yield
     finally:
-        print "%.2f ms" % ((clock()-start)*1000)
+        print("%.2f ms" % ((clock()-start)*1000))
+
 
 class StatValue:
-    def __init__(self, smooth_coef = 0.5):
+    def __init__(self, smooth_coef=0.5):
         self.value = None
         self.smooth_coef = smooth_coef
+
     def update(self, v):
         if self.value is None:
             self.value = v
         else:
             c = self.smooth_coef
             self.value = c * self.value + (1.0-c) * v
+
 
 class RectSelector:
     def __init__(self, win, callback):
@@ -176,8 +186,9 @@ class RectSelector:
         cv2.setMouseCallback(win, self.onmouse)
         self.drag_start = None
         self.drag_rect = None
+
     def onmouse(self, event, x, y, flags, param):
-        x, y = np.int16([x, y]) # BUG
+        x, y = np.int16([x, y])  # BUG
         if event == cv2.EVENT_LBUTTONDOWN:
             self.drag_start = (x, y)
         if self.drag_start:
@@ -194,28 +205,34 @@ class RectSelector:
                 self.drag_rect = None
                 if rect:
                     self.callback(rect)
+
     def draw(self, vis):
         if not self.drag_rect:
             return False
         x0, y0, x1, y1 = self.drag_rect
         cv2.rectangle(vis, (x0, y0), (x1, y1), (0, 255, 0), 2)
         return True
+
     @property
     def dragging(self):
         return self.drag_rect is not None
 
 
 def grouper(n, iterable, fillvalue=None):
-    '''grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx'''
+    # grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx
     args = [iter(iterable)] * n
     return it.izip_longest(fillvalue=fillvalue, *args)
 
-def mosaic(w, imgs):
-    '''Make a grid from images.
+
+'''
+    Make a grid from images.
 
     w    -- number of grid columns
     imgs -- images (must have same size and format)
-    '''
+'''
+
+
+def mosaic(w, imgs):
     imgs = iter(imgs)
     img0 = imgs.next()
     pad = np.zeros_like(img0)
@@ -223,15 +240,18 @@ def mosaic(w, imgs):
     rows = grouper(w, imgs, pad)
     return np.vstack(map(np.hstack, rows))
 
+
 def getsize(img):
     h, w = img.shape[:2]
     return w, h
 
+
 def mdot(*args):
     return reduce(np.dot, args)
 
-def draw_keypoints(vis, keypoints, color = (0, 255, 255)):
+
+def draw_keypoints(vis, keypoints, color=(0, 255, 255)):
     for kp in keypoints:
-            x, y = kp.pt
-            cv2.circle(vis, (int(x), int(y)), 2, color)
+        x, y = kp.pt
+        cv2.circle(vis, (int(x), int(y)), 2, color)
 
